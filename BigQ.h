0708cf032aeb5f63e18f98bed2e7881f54a28bc7
patyogesh <patyogesh@gmail.com>
2014-03-04 20:47:26 -0500

    second commit

--- BigQ.h
+++ BigQ.h
@@ -1,4 +1,4 @@
-#ifndef BIGQ_H
+#ifndef BIGQ_H 
 #define BIGQ_H
 #include <pthread.h>
 #include <iostream>
@@ -8,11 +8,58 @@
 
 using namespace std;
 
+/*
+ * Use temporary structure for passing multiple aruguments
+ * to worker thread
+ */
+struct threadParams {
+  Pipe *inPipe;
+  Pipe *outPipe;
+  OrderMaker *sortOrder;
+  int runLen;
+#ifdef DEBUG
+  Schema *schema;
+#endif
+};
+typedef struct threadParams threadParams_t;
+
+class recOnVector {
+  public:
+    Record *currRecord;
+    int    currPageNumber;
+    int    currRunNumber;
+
+
+
+    recOnVector();
+    ~recOnVector();
+};
+
 class BigQ {
 
 public:
 
-	BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen);
+
+#ifdef DEBUG
+	Schema *schema;
+#endif
+  Pipe *inPipe;
+  Pipe *outPipe;
+  OrderMaker *sortOrder;
+  int runLen;
+
+#ifdef DEBUG
+	BigQ (Pipe &in, 
+        Pipe &out, 
+        OrderMaker &sortorder, 
+        int runlen,
+        Schema *schema);
+#else
+	BigQ (Pipe &in, 
+        Pipe &out, 
+        OrderMaker &sortorder, 
+        int runlen);
+#endif
 	~BigQ ();
 };
 
